d289bb3d688ec1f38fd4f78ea9e36feab30988a4
Felipe Llerena Catalan <llerenafelipe7@gmail.com>
2020-07-09 22:03:32 -0500

    fix build prod

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,10 @@
 const common = require('./webpack.common.js')
 const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractSass = new ExtractTextPlugin({
-  filename: 'dist/css/[name].[contentHash].css'
+const extractSass = new MiniCssExtractPlugin({
+  filename: 'dist/css/[name].[hash].css'
 })
 
 module.exports = merge(common, {
@@ -13,18 +14,20 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: extractSass.extract({
-           use: [
-             {loader: 'css-loader', options: {minimize: true}},
-             {loader: 'sass-loader'}
-           ]
-        })
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          // 'postcss-loader',
+          'sass-loader',
+        ]
       },
       {
         test: /\.html$/,
         use: [
-          {loader: 'html-loader', options: {minimize: true, attrs: false}}
+          {loader: 'html-loader', options: {minimize: true}}
         ]
       }
     ]
